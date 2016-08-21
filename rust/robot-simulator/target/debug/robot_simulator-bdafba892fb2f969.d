3ae716a1c0bc02e69f8f8f764385c336bc83e9ca
Jake Goldsborough <rjgoldsborough@gmail.com>
2016-08-20 21:51:51 -0700

    removing debug stuff and adding .gitignore

--- rust/robot-simulator/target/debug/robot_simulator-bdafba892fb2f969.d
+++ rust/robot-simulator/target/debug/robot_simulator-bdafba892fb2f969.d
@@ -1,5 +1,5 @@
-/Users/ducks/exercism/rust/robot-simulator/target/debug/robot_simulator-bdafba892fb2f969: src/lib.rs
-
 /Users/ducks/exercism/rust/robot-simulator/target/debug/robot_simulator-bdafba892fb2f969.d: src/lib.rs
+
+/Users/ducks/exercism/rust/robot-simulator/target/debug/robot_simulator-bdafba892fb2f969: src/lib.rs
 
 src/lib.rs:
